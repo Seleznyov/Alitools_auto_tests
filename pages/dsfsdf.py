1e567bff9b70f876d245e69617f5dbdbf8428f74
stas <58526725+Seleznyov@users.noreply.github.com>
2023-01-05 16:03:42 +0300

    Delete dsfsdf.py

--- pages/dsfsdf.py
+++
@@ -1,8 +0,0 @@
-x = "3 185,55 руб."
-price = x.translate({ord(i): None for i in ' руб.$€¥₽US'})
-print(price)
-price = price.replace(' ', '')
-print(price)
-price = price.replace(",", ".")
-print(price)
-print(float(price))
